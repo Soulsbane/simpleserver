244979b91753c3145c6e4f5a677fb466188fb169
Paul Crane <paul@acheronsoft.com>
2018-08-17 21:19:57 -0500

    Add client module.

--- source/simpleserver/client.d
+++ source/simpleserver/client.d
@@ -1,1 +1,31 @@
 module simpleserver.client;
+
+import std.stdio;
+import std.socket;
+
+class SimpleClient
+{
+	this()
+	{
+		client_ = new TcpSocket();
+	}
+
+	void connect(ushort port = 5899)
+	{
+		client_.connect(new InternetAddress(port));
+	}
+
+	void send(const string message)
+	{
+		client_.send(message);
+	}
+
+	void disconnect()
+	{
+		client_.shutdown(SocketShutdown.BOTH);
+		client_.close();
+	}
+
+private:
+	Socket client_;
+}
