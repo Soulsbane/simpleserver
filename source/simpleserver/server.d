3ad7eacf9a0c871a2d5be0e484861636c89ebb07
Paul Crane <paul@acheronsoft.com>
2018-09-17 21:40:59 -0500

    Add documentation for simpleserver module.

--- source/simpleserver/server.d
+++ source/simpleserver/server.d
@@ -6,6 +6,7 @@ import std.conv;
 
 class SimpleServer
 {
+	///Basic setup
 	this()
 	{
 		server_ = new TcpSocket();
@@ -15,35 +16,46 @@ class SimpleServer
 		server_.listen(1);
 	}
 
+	/**
+		Starts the server using port 5899 by default.
+	*/
 	void start()
 	{
 		isRunning_ = true;
 
-		while(isRunning_)
+		while (isRunning_)
 		{
 			Socket client = server_.accept();
 			char[BUFFER_SIZE] buffer;
 			auto received = client.receive(buffer);
-			string msg = to!string(buffer[0..received]);
+			string msg = to!string(buffer[0 .. received]);
 
 			onMessage(client, msg);
 			client.shutdown(SocketShutdown.BOTH);
 			client.close();
-
-			//onMessage(msg);
 		}
 	}
 
+	/**
+		Stops the server.
+	*/
 	void stop()
 	{
 		isRunning_ = false;
 	}
 
+	/**
+		Handles all messages sent to the server.
+
+		Params:
+			client = The socket that send the message.
+			msg = The message that was sent.
+	*/
 	abstract void onMessage(Socket client, const string msg);
 
-	private:
-		ushort port_ = 5899;
-		immutable ushort BUFFER_SIZE = 1024;
-		TcpSocket server_;
-		bool isRunning_;
+private:
+	ushort port_ = 5899;
+	immutable ushort BUFFER_SIZE = 1024;
+	TcpSocket server_;
+	bool isRunning_;
 }
