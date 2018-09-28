f28ef972b9382532764fb6a1c4e84d43105272f9
Paul Crane <paul@acheronsoft.com>
2018-09-27 23:44:06 -0500

    Fix compilation error in tests due to API change.

--- source/simpleserver/tests.d
+++ source/simpleserver/tests.d
@@ -9,17 +9,22 @@ unittest
 {
 	class TestServer : SimpleServer
 	{
-		override void onMessage(const string msg)
+		override void onMessage(Socket client, const string msg)
 		{
 			writeln(msg);
 		}
+	}
+
+	class TestClient : SimpleClient
+	{
+		override void onMessage(const string msg) {}
 	}
 
 	auto server = new TestServer;
 	server.start();
 
 	// NOTE: Since SimpleServer blocks we will never make it here. This is more an example than a test.
-	auto client = new SimpleClient;
+	auto client = new TestClient;
 	client.connect();
 	client.send("quit");
 	server.stop();
